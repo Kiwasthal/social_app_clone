ec71f5b826ea44339134eba36d128656a4a9ab13
Steerpike <imthekoul@gmail.com>
2022-11-22 14:42:10 +0200

    tailwind config

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{ dark: darkMode }">
+    <div class="min-h-full">
+      <div
+        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+      >
+        <!-- Left sidebar -->
+
+        <!-- Main content -->
+
+        <!-- Right sidebar -->
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const darkMode = ref(true);
+</script>
