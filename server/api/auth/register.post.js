bf3eef558f0867803cc1d2096fdd299b97469d34
Steerpike <imthekoul@gmail.com>
2022-11-23 12:55:25 +0200

    Prisma Setup

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,6 +1,15 @@
+import { sendError } from 'h3';
+
 export default defineEventHandler(async e => {
   const body = await useBody(e);
 
+  const { userName, email, password, repeatPassword, name } = body;
+  if (!userName || !email || !password || !repeatPassword || !name) {
+    return sendError(
+      e,
+      createError({ statusCode: 400, statusMessage: 'Invalid params' })
+    );
+  }
   return {
     body,
   };
